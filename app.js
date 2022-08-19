383b22abd73e3d0e275c94456f3a8125c3733151
onggi <hsk1533@naver.com>
2022-08-19 19:45:28 +0900

    done

--- app.js
+++ app.js
@@ -21,7 +21,39 @@ app.get("/", (req, res) => {
   res.render("index");
 });
 
-app.post("/", (req, res) => {});
+app.post("/convert-mp3", async (req, res) => {
+  const videoId = req.body.videoID;
+  if (videoId === undefined || videoId === "" || videoId === null) {
+    return res.render("index", {
+      success: false,
+      message: "Please enter a video ID",
+    });
+  } else {
+    const fetchAPI = await fetch(
+      `https://youtube-mp36.p.rapidapi.com/dl?id=${videoId}`,
+      {
+        method: "GET",
+        headers: {
+          "x-rapidapi-key": process.env.API_KEY,
+          "x-rapidapi-host": process.env.API_HOST,
+        },
+      }
+    );
+    const fetchResponse = await fetchAPI.json();
+
+    if (fetchResponse.status === "ok")
+      return res.render("index", {
+        success: true,
+        song_title: fetchResponse.title,
+        song_link: fetchResponse.link,
+      });
+    else
+      return res.render("index", {
+        success: false,
+        message: fetchResponse.msg,
+      });
+  }
+});
 
 app.listen(PORT, () => {
   console.log(`Server started on port ${PORT}`);
